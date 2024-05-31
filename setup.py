8b653c13f52b47747b476f8983f40bbc16e712b1
powermaker450 <89703358+powermaker450@users.noreply.github.com>
2024-05-31 11:43:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup
 
 setup(
     name = "puffpan",
+    packages = ["puffpan"],
+    keywords = ["pufferpanel"],
     description = "puffpan is a small python module for interacting with a Pufferpanel daemon.",
     version = "1.0",
     author = "powermaker450",
     author_email = "contact@povario.com",
     url = "https://github.com/powermaker450/puff",
-    install_requires = ["setuptools", "requests"],
+    download_url = "https://github.com/powermaker450/puffpan/archive/refs/tags/v1.0.tar.gz"
+    install_requires = ["requests"],
     py_modules = ["pufferpy"],
     license = "MIT"
-    )
+)
