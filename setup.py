13d01093344939d79f32ecade6742216bfa53a83
powermaker450 <89703358+powermaker450@users.noreply.github.com>
2024-05-31 11:56:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version = "1.0",
     author = "powermaker450",
     author_email = "contact@povario.com",
-    url = "https://github.com/powermaker450/puff",
+    url = "https://github.com/powermaker450/puffpan",
     download_url = "https://github.com/powermaker450/puffpan/archive/refs/tags/v1.0.tar.gz",
     install_requires = ["requests"],
     py_modules = ["puffpan"],
